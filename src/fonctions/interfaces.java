98900074f090d5a3bc7192bc0f7ce912f7e2b330
l-busca <ludovic.busca@icloud.com>
2022-05-31 16:00:05 +0200

    interfaces et fonctions

---
+++ src/fonctions/interfaces.java
@@ -0,0 +1,17 @@
+package fonctions;
+
+public class interfaces {
+	
+	public static void accueil() {
+		
+	}
+
+
+	public static void connexion() {
+		
+	}
+
+	public static void inscription() {
+	
+}
+}
--- src/main/Ask.java
+++ src/main/Ask.java
@@ -2,6 +2,18 @@ package main;
 
 public class Ask {
 	
+	public static boolean connexion(String pseudo, String mdp) {
+		boolean success = false;
+		
+		return success;
+	}
+	
+	public static boolean inscription(String pseudo, String mdp) {
+boolean success = false;
+		
+		return success;
+	}
+	
 	public static void getTag(String tag) {
 		//BDD est dans la base ?
 		if (RequeteBDD.tagExist(tag)) {
