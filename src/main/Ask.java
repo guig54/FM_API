57959fa5a14e53e048524ca8c1369914b1bebaeb
l-busca <ludovic.busca@icloud.com>
2022-05-31 15:49:17 +0200

    structure valide avec reequete

--- src/main/Ask.java
+++ src/main/Ask.java
@@ -2,6 +2,17 @@ package main;
 
 public class Ask {
 	
-	
+	public static void getTag(String tag) {
+		//BDD est dans la base ?
+		if (RequeteBDD.tagExist(tag)) {
+			//SI oui get dans REqueteBDD.java
+		} else {
+			//SI non get dand REQUETEApi.java
+		}
+		
+		
+		
+		
+	}
 	
 }
--- src/main/RequeteBDD.java
+++ src/main/RequeteBDD.java
@@ -2,4 +2,9 @@ package main;
 
 public class RequeteBDD {
 
+	public static boolean tagExist(String tag) {
+		boolean exist = false;
+		//voir si existe
+		return exist;
+	}
 }
